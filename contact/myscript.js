a1b474cc0655d641bf677029695fe03f15061f00
Adam Britt <adamrussellbritt@gmail.com>
2018-07-16 17:03:26 -0500

    adding google map to contact page

---
+++ contact/myscript.js
@@ -0,0 +1,8 @@
+function myMap() {
+	var mapOptions = {
+		center: new google.maps.LatLng(43.1676109,-88.3101448,),
+		zoom: 17,
+		mapTypeId: google.maps.MapTypeId.HYBRID,
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
