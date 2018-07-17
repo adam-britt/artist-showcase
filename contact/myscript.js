64606612e0215ae5a2e78100366ab4bdf16cf485
Adam Britt <adamrussellbritt@gmail.com>
2018-07-16 21:00:35 -0500

    changed map type to hybrid

--- contact/myscript.js
+++ contact/myscript.js
@@ -1,20 +1,10 @@
-//function myMap() {
-//	var mapOptions = {
-//		center: new google.maps.LatLng(43.1676109,-88.3101448),
-//		zoom: 17,
-//		mapTypeId: google.maps.MapTypeId.HYBRID,
-//	}
-//	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//}
-
 function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(43.1676109,-88.3101448),
-        zoom: 17
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-};
-
+	var mapOptions = {
+		center: new google.maps.LatLng(43.1676109,-88.3101448),
+		zoom: 17,
+		mapTypeId: google.maps.MapTypeId.HYBRID,
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
 myMap();
 
