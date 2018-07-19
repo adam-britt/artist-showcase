d000d2e2f2022dd0338cdce37c8cbe2cb4adf300
Adam Britt <adamrussellbritt@gmail.com>
2018-07-18 23:19:17 -0500

    adjusted map zoom and contact info alignment

--- contact/myscript.js
+++ contact/myscript.js
@@ -1,7 +1,7 @@
 function myMap() {
 	var mapOptions = {
 		center: new google.maps.LatLng(43.1676109,-88.3101448),
-		zoom: 17,
+		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.HYBRID,
 	}
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
